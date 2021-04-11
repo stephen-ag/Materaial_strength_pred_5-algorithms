7ef8618f0a24951466fb7e678bdfed8c4dd5a2d9
gerald stephen <stephengoin@gmail.com>
2021-04-11 10:19:35 +0530

    app.run appended

--- main.py
+++ main.py
@@ -91,7 +91,8 @@ def trainRouteClient():
 
 port = int(os.getenv("PORT",5001))
 if __name__ == "__main__":
-    host='0.0.0.0'
-    httpd = simple_server.make_server( host,port, app)
-    print("Serving on %s %d" % ( host,port))
-    httpd.serve_forever()
+   # host='0.0.0.0'
+   # httpd = simple_server.make_server( host,port, app)
+   # print("Serving on %s %d" % ( host,port))
+   # httpd.serve_forever()
+   app.run()
